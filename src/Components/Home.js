2320c4e4ea760f83471293b76f7d928be4af3404
Vikeshrajput <114254062+Vikeshrajput@users.noreply.github.com>
2023-06-01 22:10:22 +0530

    image added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -22,9 +22,11 @@ function Home() {
       <ul className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 bg-gray-500 p-12 gap-4">
         {shows.map((show) => (
           <li key={show.id} className="bg-white p-4 mb-8 shadow-xl rounded-xl">
+            <img src={show.image?.medium} className='mx-auto' />
               <h5 className="text-xl font-bold">{show.name}</h5>
             <p>Type: {show.type}</p>
             <p>Language: {show.language}</p>
+            <h2 className='text-lg'><a href={show.url}><span className='text-blue-400 text-xl'>Visit Official Page</span></a></h2>
             <Link
               to={`/shows/${show.id}`}
               className="bg-blue-500 text-white py-2 px-4 rounded mt-4 inline-block"
